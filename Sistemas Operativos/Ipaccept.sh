58f40312eda086745a6db85ffb26bf65306ae09d
NicolasPerez96 <nicolasperezc96@gmail.com>
2020-08-18 22:54:55 -0300

    Se borraron archivos de Sistemas operativos

--- Sistemas Operativos/Ipaccept.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-iptables -L INPUT -n --line-numbers
-echo ""
-read -p "Seleccione la posicion en la que se encuentra la IP: " ipdes
-iptables -D INPUT $ipdes
-
--- Sistemas Operativos/RespLocal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -r /home/Usuarios /home/Respaldo/Usuarios-$(date +%d-%m-%H:%M)
-cp -r /home/administrador /home/Respaldo/Script-$(date +%d-%m-%H:%M)
